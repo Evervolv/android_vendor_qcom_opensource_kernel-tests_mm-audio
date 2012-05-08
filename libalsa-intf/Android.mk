8e831804a452b45ba796c7be047011f2c4bf7304
Ethan Chen <intervigil@gmail.com>
2012-05-08 01:15:24 -0700

    mm-audio: update build for CM9

--- libalsa-intf/Android.mk
+++ libalsa-intf/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
+ifeq ($(BOARD_USES_ALSA_AUDIO),true)
 # Any prebuilt files with default TAGS can use the below:
 include $(CLEAR_VARS)
 LOCAL_MODULE       := HiFi
@@ -183,8 +183,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= aplay.c
 LOCAL_MODULE:= aplay
 LOCAL_SHARED_LIBRARIES:= libc libcutils libalsa-intf
-LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE_TAGS:= debug
 include $(BUILD_EXECUTABLE)
 
@@ -193,8 +191,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= arec.c
 LOCAL_MODULE:= arec
 LOCAL_SHARED_LIBRARIES:= libc libcutils libalsa-intf
-LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE_TAGS:= debug
 include $(BUILD_EXECUTABLE)
 
@@ -202,8 +198,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= amix.c
 LOCAL_MODULE:= amix
 LOCAL_SHARED_LIBRARIES := libc libcutils libalsa-intf
-LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE_TAGS:= debug
 include $(BUILD_EXECUTABLE)
 
@@ -216,18 +210,16 @@ LOCAL_SRC_FILES:= alsa_mixer.c alsa_pcm.c alsa_ucm.c
 LOCAL_MODULE:= libalsa-intf
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES:= libc libcutils #libutils #libmedia libhardware_legacy
-LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-ifeq ($(strip $(QC_PROP)),true)
+ifeq ($(QC_PROP),true)
 LOCAL_CFLAGS := -DQC_PROP
 LOCAL_SHARED_LIBRARIES += libacdbloader
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-acdb-util
 endif
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 ifeq ($(TARGET_SIMULATOR),true)
- LOCAL_LDLIBS += -ldl
+    LOCAL_LDLIBS += -ldl
 else
- LOCAL_SHARED_LIBRARIES += libdl
+    LOCAL_SHARED_LIBRARIES += libdl
 endif
 LOCAL_PRELINK_MODULE := false
 include $(BUILD_SHARED_LIBRARY)
-endif
+endif # BOARD_USES_ALSA_AUDIO
